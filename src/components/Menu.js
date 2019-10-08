34106f72d35c53499268800feaae6018bee85745
Jairo Cobo <jairo_cobo@outlook.es>
2019-10-08 13:00:21 -0500

    Starting project

---
+++ src/components/Menu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import '../assets/css/components/Menu.css'
+
+import { Button } from 'react-bootstrap'
+
+const Menu = () => {
+    return (
+        <nav>
+            <div className="Avatar">
+                sdfsdffd
+            </div>
+            <div className="MenuFast">
+                <Button variant="outline-danger" block>Danger</Button>
+                <Button variant="outline-danger" block>Danger</Button>
+            </div>
+            <div className="MenuList">
+                
+            </div>
+        </nav>
+    )
+}
+
+export default Menu
---
+++ src/pages/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+import Menu from '../components/Menu'
+
+import {Row, Col} from 'react-bootstrap'
+
+import '../assets/css/pages/App.css'
+
+const App = () => {
+  return (
+    <Row className="App">
+      <Col sm={2} className="Menu"><Menu /></Col>
+      <Col sm={4}>sm=4</Col>
+      <Col sm={6}>sm=4</Col>
+    </Row>
+  )
+}
+
+export default App
